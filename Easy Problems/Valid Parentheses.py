ce22bb1da293a0d3bb1e622919934d06eaee0f99
Vikingdev13 <vikingdeveloper13@gmail.com>
2020-01-21 20:56:37 -0500

    Added Time and Space complexity messages with different variations of solutions

--- Easy Problems/Valid Parentheses.py
+++ Easy Problems/Valid Parentheses.py
@@ -1,3 +1,7 @@
+'''Runtime: 44 ms, faster than 6.07% of Python3 online submissions for Valid Parentheses.
+Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions for Valid Parentheses.'''
+
+
 class Solution:
     def isValid(self, s: str) -> bool:
         openBrackets = "([{"
@@ -5,14 +9,51 @@ class Solution:
         matchingBrackets = {')': '(', ']': '[', '}': '{'}
         stack = []
 
-        for char in s:
-            if char in openBrackets:
-                stack.append(char)
-            elif char in closeBrackets:
+        for c in s:
+            if c in openBrackets:
+                stack.append(c)
+            elif c in closeBrackets:
                 if len(stack) == 0:
                     return False
-                if stack[-1] == matchingBrackets[char]:
+                if stack[-1] == matchingBrackets[c]:
                     stack.pop()
                 else:
                     return False
         return len(stack) == 0
+
+'''Runtime: 56 ms, faster than 6.07% of Python3 online submissions for Valid Parentheses.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Valid Parentheses.'''
+
+def is_matched(s):
+    lefty = "([{"
+    righty = ")]}"
+    stack = []
+
+    for c in s:
+        if c in lefty:
+            stack.append(c)
+        elif c in righty:
+            if len(stack) == 0:
+                return False
+            if righty.index(c) != lefty.index(stack.pop()):
+                return False
+    return len(stack) == 0
+
+'''Runtime: 40 ms, faster than 10.57% of Python3 online submissions for Valid Parentheses.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Valid Parentheses'''
+
+def is_matched2(s):
+    lefty = "([{"
+    righty = ")]}"
+    stack = []
+
+    for c in s:
+        if c in lefty:
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return False
+            popped = stack.pop()
+            if lefty.index(popped) != righty.index(c):
+                return False
+    return len(stack) == 0
